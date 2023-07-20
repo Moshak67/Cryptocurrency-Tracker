a6c4bad589a0aaf9e4d7704146d4d07773227af0
Mohammad Shakir <Moshak67@gmail.com>
2023-07-20 22:42:21 +1000

    added home component to the app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navigation from "./components/Navigation";
+import Home from "./Routes/Home";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+        <Home/>
     </div>
   );
 }
